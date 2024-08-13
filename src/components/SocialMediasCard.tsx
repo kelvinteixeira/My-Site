c477df4a0e93fbd477171e312b24f65d7c42352e
kelvin teixeira <kelvin.teixeira.dev@gmail.com>
2024-08-13 12:45:28 -0300

    feat: add social medias link

--- src/components/SocialMediasCard.tsx
+++ src/components/SocialMediasCard.tsx
@@ -5,11 +5,14 @@ import {
   AiOutlineWhatsApp,
 } from "react-icons/ai";
 export const SocialMediasCard = () => {
+  const handleWhatsAppButtonClick = () => {
+    window.open("https://api.whatsapp.com/send?phone=5583996157313");
+  };
   return (
     <>
       <Grid container justifyContent={"space-evenly"} marginBottom={2}>
         <IconButton
-          href={""}
+          href={"https://www.instagram.com/kelvinteixeira_/"}
           target="_blank"
           aria-label="instagram"
           size="medium"
@@ -23,7 +26,7 @@ export const SocialMediasCard = () => {
           <AiOutlineInstagram color="#f9a72e" className="btc-white" />
         </IconButton>
         <IconButton
-          href={""}
+          href={"https://www.linkedin.com/in/kelvin-teixeira-8707b41a8/"}
           target="_blank"
           aria-label="linkedin"
           size="medium"
@@ -37,6 +40,7 @@ export const SocialMediasCard = () => {
         </IconButton>
         <IconButton
           href={""}
+          onClick={handleWhatsAppButtonClick}
           target="_blank"
           aria-label="whatsapp"
           size="medium"
@@ -46,7 +50,7 @@ export const SocialMediasCard = () => {
             },
           }}
         >
-          <AiOutlineWhatsApp color="#f9a72e" className="btc-white"/>
+          <AiOutlineWhatsApp color="#f9a72e" className="btc-white" />
         </IconButton>
       </Grid>
     </>
